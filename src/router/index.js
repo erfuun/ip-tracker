8d828ff3cff239c2e108f98dfe2342f131f1cc5c
Erfansadeghinezhad <73812978+efnninja@users.noreply.github.com>
2021-08-29 01:30:48 +0430

    😉 Fix The Title

--- src/router/index.js
+++ src/router/index.js
@@ -5,13 +5,21 @@ const routes = [
   {
     path: "/",
     name: "Home",
-    component: Home
+    component: Home,
+    meta: {
+      title: "IP Address Tracker"
+    }
   }
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+});
+
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title;
+  next();
 });
 
 export default router;
