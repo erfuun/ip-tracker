17abbeab8c1135168c5640b784fc8f0ac2a0dc47
Erfansadeghinezhad <73812978+efnninja@users.noreply.github.com>
2021-08-29 01:33:21 +0430

    🙃 Make Search input and apply a ninjas background

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,40 @@
 <template>
-  <div class="home"></div>
+  <div class="flex flex-col h-screen max-h-screen">
+    <!-- Search / Results -->
+    <div
+      class="z-20 flex justify-center relative bg-hero-pattern bg-cover px-4 pt-8 pb-32"
+    >
+      <!-- Search Input -->
+      <div class="w-full max-w-screen-sm">
+        <h1 class="text-white text-center text-3xl pb-4">IP Address Tracker</h1>
+        <div class="flex">
+          <input
+            class="flex-1 py-3 px-2 rounded-tl-md rounded-bl-md focus:outline-none"
+            type="text"
+            placeholder="Search for any IP address or leave empty to get your ip info"
+          />
+          <i
+            class="flex items-center px-4 bg-black text-white rounded-tr-md rounded-br-md pb-1 cursor-pointer"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6"
+              fill="none"
+              viewBox="0 0 20 20"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M9 5l7 7-7 7"
+              />
+            </svg>
+          </i>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
