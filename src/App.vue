25293b6f4b38f0682acef43f01196e162d871e88
Erfansadeghinezhad <73812978+efnninja@users.noreply.github.com>
2021-08-29 01:32:05 +0430

    🤩 reset styles and Nunito font

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,13 @@
 <template>
   <router-view />
 </template>
+
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Nunito:wght@600&display=swap");
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Nunito", sans-serif;
+}
+</style>
