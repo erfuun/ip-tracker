6824c838e1d1691f08eb3b899ae53094f7f510e8
Erfansadeghinezhad <73812978+efnninja@users.noreply.github.com>
2021-08-30 01:26:48 +0430

    😎 Create IPInfo Component

---
+++ src/components/IPInfo.vue
@@ -0,0 +1,27 @@
+<script setup></script>
+<template>
+  <div
+    class="absolute -bottom-64 flex flex-col lg:flex-row lg:-bottom-20 bg-white gap-x-12 gap-y-4 px-8 pt-8 pb-12 rounded-md"
+  >
+    <!-- Flex Item #1 -->
+    <div class="flex flex-col">
+      <h3 class="text-xs uppercase mb-2">IP Address</h3>
+      <span class="text-2xl">8.8.8.8</span>
+    </div>
+    <!-- Flex Item #2 -->
+    <div class="flex flex-col">
+      <h3 class="text-xs uppercase mb-2">Location</h3>
+      <span class="text-2xl">New York</span>
+    </div>
+    <!-- Flex Item #3 -->
+    <div class="flex flex-col">
+      <h3 class="text-xs uppercase mb-2">Timezone</h3>
+      <span class="text-2xl">UTC -07:00</span>
+    </div>
+    <!-- Flex Item #4 -->
+    <div class="flex flex-col">
+      <h3 class="text-xs uppercase mb-2">ISP</h3>
+      <span class="text-2xl capitalize">google</span>
+    </div>
+  </div>
+</template>
