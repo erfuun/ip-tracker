141a6a323214f1d9d14ba37d91b4716afde604f6
Erfansadeghinezhad <73812978+efnninja@users.noreply.github.com>
2021-09-03 02:04:21 +0430

    Update with reactive props value

--- src/components/IPInfo.vue
+++ src/components/IPInfo.vue
@@ -1,4 +1,9 @@
-<script setup></script>
+<script setup>
+import { defineProps } from "vue";
+const props = defineProps({
+  ipInfo: Object
+});
+</script>
 <template>
   <div
     class="absolute -bottom-64 flex flex-col lg:flex-row lg:-bottom-20 bg-white gap-x-12 gap-y-4 px-8 pt-8 pb-12 rounded-md"
@@ -6,22 +11,22 @@
     <!-- Flex Item #1 -->
     <div class="flex flex-col">
       <h3 class="text-xs uppercase mb-2">IP Address</h3>
-      <span class="text-2xl">8.8.8.8</span>
+      <span class="text-2xl">{{ props.ipInfo.address }}</span>
     </div>
     <!-- Flex Item #2 -->
     <div class="flex flex-col">
       <h3 class="text-xs uppercase mb-2">Location</h3>
-      <span class="text-2xl">New York</span>
+      <span class="text-2xl">{{ props.ipInfo.state }}</span>
     </div>
     <!-- Flex Item #3 -->
     <div class="flex flex-col">
       <h3 class="text-xs uppercase mb-2">Timezone</h3>
-      <span class="text-2xl">UTC -07:00</span>
+      <span class="text-2xl">UTC {{ props.ipInfo.timezone }}</span>
     </div>
     <!-- Flex Item #4 -->
     <div class="flex flex-col">
       <h3 class="text-xs uppercase mb-2">ISP</h3>
-      <span class="text-2xl capitalize">google</span>
+      <span class="text-2xl capitalize">{{ props.ipInfo.isp }}</span>
     </div>
   </div>
 </template>
